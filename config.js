79a67ed1f6b3dc6a978dfaec8887fec54ae4f73f
latshow <latchere.y@gmail.com>
2023-03-09 00:30:16 +0100

    feat(Command): Create command server-info.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
+    color: 0x70db6C,
     intents: 46607
 }
---
+++ src/commands/general/server-info.js
@@ -0,0 +1,26 @@
+module.exports = {
+    run: ({client, interaction}) => {
+        interaction.reply({
+            embeds: [{
+                color: client.config.color,
+                author: {name: interaction.guild.name, icon_url: interaction.guild.iconURL()},
+                thumbnail: {url: interaction.guild.iconURL({size: 2048, extension: 'png'})},
+                fields: [
+                    {name: "Propriétaire", value: `<@${interaction.guild.ownerId}>(\`${interaction.guild.ownerId}\`)`},
+                    {name: "Description", value: interaction.guild.description || "Aucune description"},
+                    {
+                        name: "Boosts",
+                        value: `${interaction.guild.premiumSubscriptionCount} Boost${interaction.guild.premiumSubscriptionCount > 1 ? '0' : ''}(\`Niveau ${interaction.guild.premiumTier}'\`)`
+                    },
+                    {name: "Membres", value: interaction.guild.memberCount.toString(), inline: true},
+                    {name: "Rôles", value: interaction.guild.roles.cache.size.toString(), inline: true},
+                    {name: "Salons", value: interaction.guild.channels.cache.size.toString(), inline: true},
+                    {name: "Date de création", value: `<t:${Math.floor(interaction.guild.createdAt / 1000)}:R>`},
+                ],
+                image: {url: interaction.guild.bannerURL({size: 2048, extension: 'png'})}
+            }]
+        })
+    }, help: {
+        description: "Afficher les informations du serveur"
+    }
+}
